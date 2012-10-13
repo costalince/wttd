c6570ad0ab6609f7219a6505137df3fc47b17b65
Gleydson Costa <costalince@gmail.com>
2012-10-12 21:01:45 -0300

    Nomes Capitalizados.

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -23,6 +23,11 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
 
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized.'
+        form = self.make_validated_form(name='HENRIQUE bastos')
+        self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
     def make_validated_form(self, **kwargs):
         data = dict(name='Henrique Bastos', email='henrique@bastos.net', cpf='12345678901', phone='21-96186180')
         data.update(kwargs)
