3bff24e75fe57e8d81b131b4bea713f9b57174ed
Gleydson Costa <costalince@gmail.com>
2012-10-12 21:37:24 -0300

    Separando o DDD do # do telefone.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from django.core.validators import EMPTY_VALUES
 from django import forms
 from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
@@ -6,12 +7,40 @@ from eventex.subscriptions.models import Subscription
 
 def CPFValidator(value):
     if not value.isdigit():
-        raise ValidationError(_(u'CPF deve conter apenas números'))
+        raise ValidationError(_(u'CPF deve conter apenas números'))
 
     if len(value) != 11:
-        raise ValidationError(_(u'CPF deve ter 11 números'))
+        raise ValidationError(_(u'CPF deve ter 11 números'))
+
+class PhoneWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        widgets = (forms.TextInput(attrs=attrs), forms.TextInput(attrs=attrs))
+        super(PhoneWidget, self).__init__(widgets, attrs)
+    
+    def decompress(self, value):
+        if not value:
+            return [None, None]
+        return value.split('-')
+
+class PhoneField(forms.MultiValueField):
+    widget = PhoneWidget
+
+    def __init__(self, *args, **kwargs):
+        fields = (forms.IntegerField(), forms.IntegerField())
+        super(PhoneField, self).__init__(fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        if not data_list:
+            return ''
+        if data_list[0] in EMPTY_VALUES:
+            raise forms.ValidationError(_(u'DDD inválido.'))
+        if data_list[1] in EMPTY_VALUES:
+            raise forms.ValidationError(_(u'Número inválido.'))
+        return '%s-%s' % tuple(data_list)
 
 class SubscriptionForm(forms.ModelForm):
+    phone = PhoneField(label=_('Telefone'), required=False)
+    
     class Meta:
         model = Subscription
         exclude = ('paid',)
@@ -29,6 +58,8 @@ class SubscriptionForm(forms.ModelForm):
 
     def clean(self):
         super(SubscriptionForm, self).clean()
+
         if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
-            raise ValidationError(_(u'Informe seu e-mail ou telefone'))
+                raise ValidationError(_(u'Informe seu e-mail ou telefone'))
+
         return self.cleaned_data
